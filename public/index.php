ccace88486e10c50ab71e48d617a01ab7364d9ab
jacobliwewe <liwewejacob265@gmail.com>
2026-01-14 16:49:06 +0200

    Ultra simple index.php for testing

--- public/index.php
+++ public/index.php
@@ -1,25 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// Load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
-// Manual class loading for deployment
-require_once __DIR__ . '/../app/Router.php';
-require_once __DIR__ . '/../app/helpers/Response.php';
-require_once __DIR__ . '/../app/helpers/JwtHelper.php';
-require_once __DIR__ . '/../config/Database.php';
-
-// Handle CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
-
 // Simple test endpoint
 if (strpos($_SERVER['REQUEST_URI'], '/api/test') !== false) {
     echo json_encode(['message' => 'API is working!', 'timestamp' => time()]);
