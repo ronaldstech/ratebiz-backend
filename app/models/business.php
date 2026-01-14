317f54b51179de4e9396f8000d35b2855d31bfd5
jacobliwewe <liwewejacob265@gmail.com>
2026-01-14 15:35:46 +0200

    added business model

---
+++ app/models/business.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Models;
+
+use App\Config\Database;
+use PDO;
+
+class Business
+{
+    public static function create(array $data): void
+    {
+        $db = Database::connect();
+
+        $stmt = $db->prepare("
+            INSERT INTO businesses 
+            (id, owner_id, name, category, description, location)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ");
+
+        $stmt->execute([
+            $data['id'],
+            $data['owner_id'],
+            $data['name'],
+            $data['category'],
+            $data['description'],
+            $data['location']
+        ]);
+    }
+
+    public static function all(): array
+    {
+        $db = Database::connect();
+        return $db->query("SELECT * FROM businesses WHERE is_verified = 1")->fetchAll();
+    }
+
+    public static function find(string $id): ?array
+    {
+        $db = Database::connect();
+        $stmt = $db->prepare("SELECT * FROM businesses WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch() ?: null;
+    }
+}
